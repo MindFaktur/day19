d20830a98a4069f205fe94a64ff538f87041e8c4
Vishwanath <vishwanathiti828@gmail.com>
2022-01-09 20:18:09 +0530

    added all rules for name,id,phone,password

--- src/regexProblem.java
+++ src/regexProblem.java
@@ -8,24 +8,26 @@ public class regexProblem {
 
     private static final Scanner SC = new Scanner(System.in);
 
+    private final String nameRegex = "^[A-Z][a-z]{3,}$";
+    private final String emailRegex = "^[a-zA-Z0-9][a-zA-Z0-9+_.-]+@[a-zA-Z0-9][a-zA-Z0-9]+[+_.-][a-zA-Z0-9]+$";
+    private final String mobNumberRegex = "^[0-9]{2} [6789][0-9]{9}$";
+    private final String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\\\S+$).{8,20}$";
+
     public static void main(String[] args)
     {
         regexProblem RP = new regexProblem();
 
         String firstName = RP.getInput("First Name");
-
         String lastName = RP.getInput("Last Name");
-
         String email = RP.getInput("Email-id");
-
         String phoneNumber = RP.getInput("Phone Number");
+        String password = RP.getInput("Password");
 
-        String specialCharacters = "^[a-zA-Z0-9][a-zA-Z0-9+_.-]+@[a-zA-Z0-9][a-zA-Z0-9]+[+_.-][a-zA-Z0-9]+$";
-
-        RP.printsValidOrInvalid(RP.regexChecker("^[A-Z][a-z]{3,}$",firstName),firstName,"First Name" );
-        RP.printsValidOrInvalid(RP.regexChecker("^[A-Z][a-z]{3,}$",lastName),lastName,"Last Name" );
-        RP.printsValidOrInvalid(RP.regexChecker(specialCharacters,email),email,"Email-id" );
-        RP.printsValidOrInvalid(RP.regexChecker("^[0-9]{2} [6789][0-9]{9}$",phoneNumber),phoneNumber,"Phone Number" );
+        RP.printsValidOrInvalid(RP.regexChecker(RP.nameRegex,firstName),firstName,"First Name" );
+        RP.printsValidOrInvalid(RP.regexChecker(RP.nameRegex,lastName),lastName,"Last Name" );
+        RP.printsValidOrInvalid(RP.regexChecker(RP.emailRegex,email),email,"Email-id" );
+        RP.printsValidOrInvalid(RP.regexChecker(RP.mobNumberRegex,phoneNumber),phoneNumber,"Phone Number" );
+        RP.printsValidOrInvalid(RP.regexChecker(RP.passwordRegex,password),password,"Password" );
 
     }
 

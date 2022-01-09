406aa282c29a4bd28930f6f453f2417de419a6ad
Vishwanath <vishwanathiti828@gmail.com>
2022-01-09 16:21:12 +0530

    added uc3 i.e verify phone number

--- src/regexProblem.java
+++ src/regexProblem.java
@@ -12,21 +12,27 @@ public class regexProblem {
     {
         regexProblem RP = new regexProblem();
 
-        System.out.println("Please enter your first Name");
-        String firstName = SC.next();
+        String firstName = RP.getInput("First Name");
 
-        System.out.println("Please enter your Last Name");
-        String lastName = SC.next();
+        String lastName = RP.getInput("Last Name");
 
-        System.out.println("Please enter your Email");
-        String email = SC.next();
+        String email = RP.getInput("Email-id");
+
+        String phoneNumber = RP.getInput("Phone Number");
 
         String specialCharacters = "^[a-zA-Z0-9][a-zA-Z0-9+_.-]+@[a-zA-Z0-9][a-zA-Z0-9]+[+_.-][a-zA-Z0-9]+$";
 
-        RP.printsValidOrInvalid(RP.regexChecker("^[A-Z][a-z]{3,}",firstName),firstName,"First Name" );
-        RP.printsValidOrInvalid(RP.regexChecker("^[A-Z][a-z]{3,}",lastName),lastName,"Last Name" );
+        RP.printsValidOrInvalid(RP.regexChecker("^[A-Z][a-z]{3,}$",firstName),firstName,"First Name" );
+        RP.printsValidOrInvalid(RP.regexChecker("^[A-Z][a-z]{3,}$",lastName),lastName,"Last Name" );
         RP.printsValidOrInvalid(RP.regexChecker(specialCharacters,email),email,"Email-id" );
+        RP.printsValidOrInvalid(RP.regexChecker("^[0-9]{2} [6789][0-9]{9}$",phoneNumber),phoneNumber,"Phone Number" );
 
+    }
+
+    private String getInput(String userField)
+    {
+        System.out.println("Please enter your " + userField);
+        return SC.nextLine();
     }
 
     private void printsValidOrInvalid(boolean val, String name, String userField){
